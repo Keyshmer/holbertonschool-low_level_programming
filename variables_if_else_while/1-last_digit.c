b1cb46664059ae4fae1e99f9147bab71c0f0f7b0
Keyshmer <velazquez.keyshmer@gmail.com>
2023-10-16 11:09:10 -0400

    kuj

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <studio.h>
+/**
+ *  main - text or printf accoring to the number.
+ *
+ * Return: always (success)
+ **/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf(n % 10);
+
+	if (last > 5)
+	{
+		printf(" and is greater than 5\n");
+	}
+	else
+	{
+		if (last == 0)
+		{
+			printf(" and is 0\n");
+		}
+		else
+		{
+		if (last < 6 && last != 0)
+		{
+			printf(" and is less than 6 and not 0\n");
+		}
+		}
+	}
+	return (0);
+}
