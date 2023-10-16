d45d63e701092d884f1dc5075ba57e36ff388134
Keyshmer <velazquez.keyshmer@gmail.com>
2023-10-16 15:18:30 -0400

    hygh

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all number in lowercase
+ *
+ * Return: 0 success
+ **/
+int main(void)
+{
+	int h, low;
+
+	for (h = 0; h <= 9; h++)
+	{
+	putchar(h + '0');
+	}
+	for (low = a; low <= f; low++)
+	{
+	putchar(low);
+	}
+	putchar('\n')
+	return (0);
+}
