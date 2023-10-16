6649a3af3248623cc3829ae465023a79a935536c
Keyshmer <velazquez.keyshmer@gmail.com>
2023-10-16 14:00:42 -0400

    gtf

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return:0 success
+ **/
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != 'e' && low != 'q')
+	}
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
