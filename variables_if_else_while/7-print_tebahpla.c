6c2cae531e1432b7007e6531b1cf20bf185a8bdc
Keyshmer <velazquez.keyshmer@gmail.com>
2023-10-16 14:57:18 -0400

    HYJ

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - program tha prints in lowercase in reverse
+ *
+ * Return: 0 success
+ **/
+int main(void)
+{
+	int t;
+
+	for (t = 'z'; t >= 'a'; t--)
+	{
+	putchar((t) - '0');
+	}
+	putchar('\n');
+	return (0);
+}
