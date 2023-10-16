746388a865395eae8103529fa75ec4f2a26f7cf2
Keyshmer <velazquez.keyshmer@gmail.com>
2023-10-16 13:29:28 -0400

    jyt

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry points
+ *
+ * Return: 0 success
+ **/
+int main(void)
+{
+	char low;
+
+	for (low - 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
