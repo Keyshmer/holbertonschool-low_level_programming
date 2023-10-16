fa7a565fa890e90cb420dc3750a55d84560cb1c0
Keyshmer <velazquez.keyshmer@gmail.com>
2023-10-16 11:45:03 -0400

    juh

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ * main - prints the alphabetic
+ *
+ * Return: 0 is success
+ **/
+int main(void)
+{
+	int n, k;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	char c;
+
+	for (c = 'a' ; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+
+	return (0);
+}
